a7f4485d5593967b91dbda42848074c256d7cfb6
Mustafa Alkamali <m7asanain@gmail.com>
2023-11-06 20:55:39 +0300

    the events modulesand eventEmitter class

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-const tutorial = require("./tutorial");
+const { log } = require('console');
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
 
-console.log(tutorial.sum(1, 1));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject);
+eventEmitter.on("tutorial", (num1, num2) => {
+    console.log(num1 + num2);
+});
+
+eventEmitter.emit('tutorial', 1, 2);
+
+class Person extends EventEmitter {
+    constructor(name) {
+        super();
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
+
+let pedro = new Person("Pedro");
+
+pedro.on('name', () => {
+    console.log("My name is", pedro.name);
+});
+
+pedro.emit('name');
+
+// second preson
+let christina = new Person("Christina");
+
+christina.on("name", () => {
+    console.log("My name is " + christina.name);
+});
+
+christina.emit("name");
--- tutorial.js
+++
@@ -1,9 +0,0 @@
-const sum = (num1, num2) => num1 + num2;
-const PI = 3.14;
-class SomeMathObject {
-    constructor() {
-        console.log('Object Created!');
-    }
-}
-
-module.exports = { sum : sum, PI : PI, SomeMathObject : SomeMathObject };
